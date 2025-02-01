684bd1d5357e7aec67a552d0c6e872d121720c4e
Atharva-Puranik <atharvap887@gmail.com>
2025-02-01 18:58:20 +0530

    DATABASE CONNECTION

--- src/main/java/com/dens/DensProjectApplication.java
+++ src/main/java/com/dens/DensProjectApplication.java
@@ -2,8 +2,10 @@ package com.dens;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class DensProjectApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/dens/databeans/User.java
@@ -0,0 +1,60 @@
+package com.dens.databeans;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "_users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "user_prime_key")
+	private String userPrimeKey;
+	
+	@Column(name = "username")
+	private String userName;
+	
+	@Column(name = "passoword")
+	private String password;
+	
+	@Column(name = "otp_requested_time")
+	private String otp_requested_time;
+
+	public String getUserPrimeKey() {
+		return userPrimeKey;
+	}
+
+	public void setUserPrimeKey(String userPrimeKey) {
+		this.userPrimeKey = userPrimeKey;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getOtp_requested_time() {
+		return otp_requested_time;
+	}
+
+	public void setOtp_requested_time(String otp_requested_time) {
+		this.otp_requested_time = otp_requested_time;
+	}
+	
+}
