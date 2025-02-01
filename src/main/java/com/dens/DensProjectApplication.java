3625e404eaa8f51ae40b634fdf087df2b3b6cf80
Atharva-Puranik <atharvap887@gmail.com>
2025-02-01 15:34:46 +0530

    INITIAL COMMIT

---
+++ src/main/java/com/dens/DensProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DensProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DensProjectApplication.class, args);
+	}
+
+}
